9cc4c5caed34064a10f85be46ecddc9f384026f7
钟杰 <891853602@qq.com>
2021-08-02 19:36:38 +0800

    feat(components): 按钮权限指令开发

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,24 @@ import 'element-plus/lib/theme-chalk/index.css'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import storage from './utils/storage'
 
 const app = createApp(App)
 
 app.use(router).use(store).use(ElementPlus, { size: 'small' }).mount('#app')
+
+app.directive('has', {
+  beforeMount: (el, binding) => {
+    // 获取按钮权限
+    const actionList = storage.getItem('actionList')
+    const { value } = binding
+    // 判断列表中是否有对应的按钮权限标识
+    const hasPermission = actionList.includes(value)
+    if (!hasPermission) {
+      el.style = 'display:none'
+      setTimeout(() => {
+        el.parentNode.removeChild(el)
+      }, 0)
+    }
+  }
+})
